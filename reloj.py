0ae5967456ad24604108b966b2740ad8e8b2bee2
Marina Arriaza <marinarriazaurrea@gmail.com>
2024-12-19 18:15:06 -0300

    Agregando formato al reloj

--- reloj.py
+++ reloj.py
@@ -16,21 +16,28 @@ def actualizar_reloj():
 
 app = Tk()
 app.title("Reloj Dijital")
+app.config(background="black")
 
 #Etiquetas de horas, minutos y segundos
 
-frame_hora=Frame()
+frame_hora = Frame()
 frame_hora.pack()
-etiqueta_hm=Label(frame_hora, font=("Digitalk", 100), text="H:M")
+etiqueta_hm = Label(frame_hora, font=("digital-7", 100), foreground="cyan", background="black", text="H:M")
 etiqueta_hm.grid(row=0, column=0)
 
-etiqueta_s=Label(frame_hora, font=("digitalk", 50), text="s")
+etiqueta_s = Label(frame_hora, font=("digital-7", 50), foreground="cyan", background="black", text="s")
 etiqueta_s.grid(row=0, column=1, sticky="n")
 
 #Etiqueta de fecha y su ubicación
 
-etiqueta_fecha=Label(font=("Digitalk", 50), text="día d/m/aaaa")
+etiqueta_fecha = Label(app, font=("digital-7", 50), foreground="cyan", background="black", text="día d/m/aaaa")
 etiqueta_fecha.pack(anchor="center")
+
+#Efecto de brillo
+
+brillo = Label(app, background="white", foreground="white", text="", font=("digital-7", 100))
+brillo.place(x=etiqueta_hm.winfo_x(), y=etiqueta_hm.winfo_y())
+brillo.after(500, lambda: brillo.place_forget())
 
 #Mostrando ventana y llamar reloj
 
